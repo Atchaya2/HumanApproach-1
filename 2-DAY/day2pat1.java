cea779e9d6f2b04c4ec30ed0249034021845b69c
Atchaya2 <139252097+Atchaya2@users.noreply.github.com>
2023-07-16 16:11:29 +0530

    Add files via upload

---
+++ 2-DAY/day2pat1.java
@@ -0,0 +1,21 @@
+package day2pat1;
+
+public class day2pat1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		      int n=5;
+		      for(int i=1;i<=n;i++)
+		      {
+		          for(int k=1;k<=n-i;k++)
+		          {
+		                System.out.print(".");
+		          }
+		          for(int j=i;j>=1;j--)
+		          {
+		                System.out.print(j);
+		          }
+		          System.out.println();
+		      }
+	    }
+}
